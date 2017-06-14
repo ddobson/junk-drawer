f8da70f94bc003302814c924a35314dca233a37c
ddobson <ddobson445@gmail.com>
2017-06-14 15:52:00 -0400

    Clear content from App component

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,12 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>App Component</h1>
       </div>
     );
   }
