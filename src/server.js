5c0a3b2b5ed19d800be5c259924b9a6dd0821158
ddobson <ddobson445@gmail.com>
2017-06-15 19:31:19 -0400

    Install & Initialize Mongoose

--- src/server.js
+++ src/server.js
@@ -1,33 +1,38 @@
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const path = require('path');
 const api = require('./server/routes/api');
+const app = express();
 
 dotenv.config();
 
-const app = express();
+// DATABASE
+mongoose.connect(process.env.MONGODB);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log(`MongoDB connected at ${process.env.MONGODB}`);
+});
 
 // MIDDLEWARE
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-
 app.use(morgan('combined'));
-
+app.use('/api', api);
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.use('/api', api);
-
 // ROUTING
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
-// Server Setup
-
+// SERVER
 const port = process.env.PORT || 3001;
 app.set('port', port);
 
