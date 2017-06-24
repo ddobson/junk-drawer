eb81d96bf863e8672e56b6de93d1258abe082353
ddobson <ddobson445@gmail.com>
2017-06-24 10:32:00 -0400

    Implement Change Password Functionality

--- src/server/controllers/authentication.js
+++ src/server/controllers/authentication.js
@@ -1,26 +1,32 @@
 const jwt = require('jwt-simple');
 const User = require('../models/user');
 
-function tokenForUser(user) {
+function _tokenForUser(user) {
   const secret = process.env.SECRET;
   const timestamp = new Date().getTime();
 
   return jwt.encode({ sub: user.id, iat: timestamp }, secret);
 }
 
-exports.signup = function (req, res, next) {
+function signup(req, res, next) {
   const { email, userName, password, passwordConf } = req.body;
 
   if (!email || !password || !passwordConf || !userName) {
-    res.status(422).json({ error: 'You must provide an email, username, password and password confirmation.' });
+    res.status(400).json({ error: 'You must provide an email, username, password and password confirmation.' });
   }
 
   if (password !== passwordConf) {
     res.status(422).json({ error: 'The password and password confirmation do not match.' });
   }
 
+  if (password.length < 8) {
+    res.status(422).json({ error: 'Password must be 8 or more characters.' });
+  }
+
   User.findOne({ email }, (err, existingUser) => {
-    if (err) return next(err);
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
 
     if (existingUser) {
       return res.status(422).json({ error: 'Email is already in user' });
@@ -31,11 +37,51 @@ exports.signup = function (req, res, next) {
     newUser.save(newUser, (err) => {
       if (err) return next(err);
 
-      return res.status(201).json({ token: tokenForUser(newUser), user: newUser });
+      return res.status(201).json({ token: _tokenForUser(newUser), user: newUser });
     });
   });
-};
+}
 
-exports.signin = function(req, res) {
-  res.send({ token: tokenForUser(req.user) });
+function signin(req, res) {
+  res.json({ token: _tokenForUser(req.user) });
+}
+
+function changePassword(req, res) {
+  const { password, newPassword } = req.body;
+
+  if (!password || !newPassword) {
+    return res.status(400).json({ error: 'You must provide both the current and new passwords.' });
+  }
+
+  if (newPassword.length < 8) {
+    return res.status(422).json({ error: 'New password must be 8 or more characters.' });
+  }
+
+  User.findById(req.user._id, (err, user) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    user.comparePassword(password, (err, isMatch) => {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+
+      if (!isMatch) {
+        return res.status(401).json({ error: 'Invalid password.' });
+      }
+
+      user.password = newPassword; // eslint-disable-line no-param-reassign
+      user.save();
+
+      res.sendStatus(204);
+    });
+  });
+}
+
+module.exports = {
+  signup,
+  signin,
+  changePassword,
+  _tokenForUser,
 };
--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -6,10 +6,13 @@ const Authentication = require('../controllers/authentication');
 const router = express.Router();
 
 passport.use(passportService.localLogin);
+passport.use(passportService.jwtLogin);
 
 const requireSignin = passport.authenticate('local', { session: false });
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 router.post('/signup', Authentication.signup);
 router.post('/signin', requireSignin, Authentication.signin);
+router.put('/changepw', requireAuth, Authentication.changePassword);
 
 module.exports = router;
