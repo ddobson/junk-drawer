da6799a6696b46526201c9a57dc267d94af8ed17
ddobson <ddobson445@gmail.com>
2017-06-24 20:50:43 -0400

    Scaffold SignInForm using redux-form - Install redux-form - Wire redux for to root reducer - Write some basic markup for signin form

---
+++ src/client/components/SignInForm.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Field, reduxForm } from 'redux-form';
+
+const SignInForm = props => (
+  <section className="section">
+    <div className="container is-fluid">
+      <h1 className="title">
+        Sign In
+      </h1>
+    </div>
+    <div className="columns">
+      <div className="column is-offset-one-quarter is-half-tablet">
+        <form onSubmit={props.handleSubmit(props.onSignInSubmit)}>
+          <div className="field">
+            <label htmlFor="email-field" className="label">Email</label>
+            <Field
+              id="email-field"
+              name="email"
+              className="input"
+              component="input"
+              type="text"
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="pass-field" className="label">Password</label>
+            <Field
+              id="pass-field"
+              name="password"
+              className="input"
+              component="input"
+              type="password"
+            />
+          </div>
+          <div className="field">
+            <button className="button" type="submit">Sign In</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
+);
+
+SignInForm.propTypes = {
+  handleSubmit: PropTypes.func,
+  onSignInSubmit: PropTypes.func,
+};
+
+export default reduxForm({ form: 'signin' })(SignInForm);
