dbaf96d3b11b9f3f50de7da9afb93c7e44792c2c
ddobson <ddobson445@gmail.com>
2017-06-26 10:53:46 -0400

    Dispatch userSignIn on form submission - Map dispatch to props and dispatch userSignIn action creator on submission - Redirect user they were not signed in and will be signed in

--- src/client/containers/Authentication.js
+++ src/client/containers/Authentication.js
@@ -4,17 +4,24 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import SignInForm from '../components/SignInForm';
+import { signInUser } from '../actions/auth';
 import '../styles/Authentication.scss';
 
 class Authentication extends Component {
-  static onSignInSubmit(values) {
-    console.log(values);
-  }
-
   constructor(props) {
     super(props);
 
     this.renderForm = this.renderForm.bind(this);
+  }
+
+  componentWillReceiveProps(nextprops) {
+    const currentlyAuthenticated = this.props.auth.isAuthenticated;
+    const willBeAuthenticated = nextprops.auth.isAuthenticated;
+    const { push } = this.props.history;
+
+    if (!currentlyAuthenticated && willBeAuthenticated) {
+      push('/');
+    }
   }
 
   renderForm() {
@@ -22,7 +29,7 @@ class Authentication extends Component {
 
     switch (pathname) {
       case '/signin':
-        return <SignInForm onSignInSubmit={Authentication.onSignInSubmit} />;
+        return <SignInForm onSignInSubmit={this.props.onSignInSubmit} />;
       default:
         return null;
     }
@@ -38,7 +45,12 @@ class Authentication extends Component {
 }
 
 Authentication.propTypes = {
+  onSignInSubmit: PropTypes.func,
+  auth: PropTypes.shape({
+    isAuthenticated: PropTypes.bool,
+  }),
   history: PropTypes.shape({
+    push: PropTypes.func,
     location: PropTypes.shape({
       pathname: PropTypes.string,
     }),
@@ -49,6 +61,12 @@ const mapStateToProps = state => (
   { auth: state.auth }
 );
 
+const mapDispatchToProps = dispatch => ({
+  onSignInSubmit(values) {
+    dispatch(signInUser(values));
+  },
+});
+
 const AuthenticationWithRouter = withRouter(Authentication);
 
-export default connect(mapStateToProps)(AuthenticationWithRouter);
+export default connect(mapStateToProps, mapDispatchToProps)(AuthenticationWithRouter);
