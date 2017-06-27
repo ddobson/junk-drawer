704b4338618a4d18240afd567d63b709bbda6955
ddobson <ddobson445@gmail.com>
2017-06-26 21:06:48 -0400

    Add async/await to signInUser - Refactor signInUser to user async/await b/c it's future and super coolz :)

--- src/client/actions/auth.js
+++ src/client/actions/auth.js
@@ -26,30 +26,27 @@ export function authError(error) {
 }
 
 export function signInUser(formData) {
-  return (dispatch) => {
-    dispatch(authError({ hasErrored: false, error: '' })); // Sanitize auth error state
-    dispatch(authIsLoading(true)); // Set app to loading
+  return async (dispatch) => {
+    try {
+      dispatch(authError({ hasErrored: false, error: '' })); // Sanitize auth error state
+      dispatch(authIsLoading(true)); // Set app to loading
 
-    const signInResponse = axios({
-      url: 'http://localhost:3001/auth/signin',
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      data: formData,
-    });
-
-    signInResponse
-      .then(response => response.data)
-      .then((data) => {
-        localStorage.setItem('token', data.token);
-        dispatch(authStatus(true));
-      })
-      .catch((error) => {
-        dispatch(authError({ hasErrored: true, error: error.response.data }));
+      const signInResponse = await axios({
+        url: 'http://localhost:3001/auth/signin',
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: formData,
       });
 
-    dispatch(authIsLoading(false));
+      localStorage.setItem('token', signInResponse.data.token);
+      dispatch(authStatus(true));
+    } catch (error) {
+      dispatch(authError({ hasErrored: true, error: error.response.data }));
+    } finally {
+      dispatch(authIsLoading(false));
+    }
   };
 }
 
