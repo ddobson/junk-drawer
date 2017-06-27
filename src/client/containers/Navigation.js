70b3663ce52c95b71aea48c564f55cde124e423d
ddobson <ddobson445@gmail.com>
2017-06-27 10:06:02 -0400

    Brand the page - Change html title - Add style the nav logo

--- src/client/containers/Navigation.js
+++ src/client/containers/Navigation.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
+import { Link } from 'react-router-dom';
 import { signOutUser } from '../actions/auth';
 import NavMenu from '../components/NavMenu';
+import '../styles/Navigation.scss';
 
 const Navigation = (props) => {
   const { isAuthenticated } = props.auth;
@@ -13,7 +14,16 @@ const Navigation = (props) => {
     <nav className="nav has-shadow">
       <div className="nav-left">
         <div className="nav-item">
-          <h3>Junk Drawer</h3>
+          <Link to="/">
+            <div>
+              <h1 className="brand">
+                <span className="icon">
+                  <i className="fa fa-inbox" />
+                </span>
+                <span className="brand-text">Junk Drawer</span>
+              </h1>
+            </div>
+          </Link>
         </div>
       </div>
       <NavMenu isAuthenticated={isAuthenticated} signOut={signOut} />
