dcb3c2f781960e1bd9e80569babaa46c6cdb6a9d
ddobson <ddobson445@gmail.com>
2017-06-28 18:30:18 -0400

    Create a reusable button component

---
+++ src/client/components/ui/Button.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+const Button = function(props) {
+  const btnStyle = classnames({
+    button: true,
+    disabled: props.isDisabled,
+    'is-danger': props.isDanger,
+    'is-loading': props.isLoading,
+    'is-primary': props.isPrimary,
+  });
+
+  return (
+    <button type={props.type} className={btnStyle} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: React.PropTypes.node.isRequired,
+  isDisabled: PropTypes.bool,
+  isDanger: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  isPrimary: PropTypes.bool,
+  onClick: PropTypes.func,
+  type: PropTypes.string,
+};
+
+export default Button;
