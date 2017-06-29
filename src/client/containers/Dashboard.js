3d68afdfc17604929faf76b956dca88f8648c5af
ddobson <ddobson445@gmail.com>
2017-06-28 21:14:29 -0400

    Connect Dashboard to redux and fetch links

--- src/client/containers/Dashboard.js
+++ src/client/containers/Dashboard.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { linksFetchData } from '../actions/links';
 
 class Dashboard extends Component {
+  componentDidMount() {
+    this.props.fetchLinks();
+  }
 
   render() {
     return (
       <div>Dashboard</div>
     );
   }
-
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+  fetchLinks: PropTypes.func,
+};
+
+const mapStateToProps = state => (
+  { links: state.links }
+);
+
+const mapDispatchToProps = {
+  fetchLinks: linksFetchData,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
