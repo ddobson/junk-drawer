8cb0f9bda4ca3c4a3f3549761c68f2573c5535ac
ddobson <ddobson445@gmail.com>
2017-07-03 14:57:31 -0400

    Display a list of links in the dashboard - New LinkListItem component - Styles for component - Map through links and display component for each one

---
+++ src/client/components/links/LinkListItem.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import '../../styles/LinkListItem.scss';
+
+class LinkListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isCollapsed: true,
+    };
+
+    this.toggleCollapse = this.toggleCollapse.bind(this);
+  }
+
+  toggleCollapse() {
+    this.setState({ isCollapsed: !this.state.isCollapsed });
+  }
+
+  render() {
+    const { originalHost, shortUrl, title } = this.props.link;
+    const wrapperStyles = classnames({
+      'link-wrapper': true,
+      collapsed: this.state.isCollapsed,
+    });
+    const iconAngleStyles = classnames({
+      fa: true,
+      'fa-angle-down': this.state.isCollapsed,
+      'fa-angle-up': !this.state.isCollapsed,
+    });
+
+    return (
+      <div className={wrapperStyles}>
+        <div className="link-url">
+          <img
+            className="og-host-icon"
+            src={`https://www.google.com/s2/favicons?domain=${originalHost}`}
+            alt={`${originalHost} icon`}
+          />
+          <a
+            href={shortUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {shortUrl}
+          </a>
+          <span className="icon" role="presentation" onClick={this.toggleCollapse}>
+            <i className={iconAngleStyles} />
+          </span>
+          <span className="icon is-small">
+            <i className="fa fa-trash-o" />
+          </span>
+          <span className="icon is-small">
+            <i className="fa fa-copy" />
+          </span>
+        </div>
+        <div className="link-info">
+          <p>{title}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+LinkListItem.propTypes = {
+  link: PropTypes.shape({
+    originalHost: PropTypes.string,
+    shortUrl: PropTypes.string,
+    title: PropTypes.string,
+  }),
+};
+
+export default LinkListItem;
--- src/client/containers/Dashboard.js
+++ src/client/containers/Dashboard.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import map from 'lodash/map';
 import { connect } from 'react-redux';
 import { linksFetchData } from '../actions/links';
 
 import LoadingSpinner from '../components/ui/LoadingSpinner';
+import LinkListItem from '../components/links/LinkListItem';
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.renderLinkListItems = this.renderLinkListItems.bind(this);
+  }
   componentDidMount() {
     this.props.fetchLinks();
+  }
+
+  renderLinkListItems() {
+    const { links } = this.props;
+
+    return map(links, link => <LinkListItem key={link._id} link={link} />);
   }
 
   render() {
@@ -24,13 +37,23 @@ class Dashboard extends Component {
     }
 
     return (
-      <div>Dashboard</div>
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+              <h1 className="title">Links</h1>
+              {this.renderLinkListItems()}
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
 
 Dashboard.propTypes = {
   fetchLinks: PropTypes.func,
+  links: PropTypes.object, // eslint-disable-line
   linksMeta: PropTypes.shape({
     isLoading: PropTypes.bool,
   }),
